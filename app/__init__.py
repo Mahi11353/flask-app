ad6a1acdcdb1e49a8b73fc2bcf1f52f61171c7f3
Mahi11353 <gupta.mahi1409@gmail.com>
2026-06-02 13:28:38 +0530

    Add deployment configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,8 +8,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'your-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///todo.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 
     db.init_app(app)
